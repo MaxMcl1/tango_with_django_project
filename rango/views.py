b4898c6d5d44c37583079cdb10a2cc7086a7ab8b
Max McLaughlan <2137949m@student.gla.ac.uk>
2018-01-16 15:34:30 +0000

    working hyperlinks

--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Maxo says Hi")
+    return HttpResponse("Hey visit about <br/> <a href='/rango/about/'>About</a>")
+
+def about(requesty):
+    return HttpResponse("Maxo says this is about <a href='/rango/'>Index</a>")
 
