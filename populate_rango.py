b61303a63d7e6498f98cd52c77871e7df53d59ca
Max McLaughlan <2137949m@student.gla.ac.uk>
2018-01-25 12:28:42 +0000

    Chapter 5

--- populate_rango.py
+++ populate_rango.py
@@ -34,9 +34,9 @@ def populate():
         {"title":"Flask",
          "url":"http://flask.pocoo.org"} ]
 
-    cats = {"Python": {"pages": python_pages},
-            "Django": {"pages": django_pages},
-            "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages, "likes": 64, "views": 128},
+            "Django": {"pages": django_pages, "likes": 32, "views": 64},
+            "Other Frameworks": {"pages": other_pages, "likes": 16, "views": 32} }
 
     # If you want to add more catergories or pages,
 
@@ -49,7 +49,7 @@ def populate():
     # for more information about how to iterate over a dictionary properly.
 
     for cat,cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
 
@@ -66,8 +66,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views, likes):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
